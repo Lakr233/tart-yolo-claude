df08bac8365d3975121f467d337c956f2964bad1
Lakr <git@qaq.wiki>
2025-11-04 14:29:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,6 @@ chmod +x *.py
 cp -f yolo_*.sh /usr/local/bin/
 cp -f yolo_*.py /usr/local/bin/
 
-PREPARED_IMAGE_NAME="tart_yolo_base"
-tart stop "$PREPARED_IMAGE_NAME" || true
-tart delete "$PREPARED_IMAGE_NAME" || true
 ./yolo_tart_prepare.sh
-rm $(which yolo_tart_prepare.sh)
 
 echo "[*] installation complete. You can now run 'yolo_claude.sh', 'yolo_codex.sh', or 'yolo_zsh.sh' from anywhere."
