831c4373e4737ed9d0e7cf2cd7bfce72343b3cf9
Lakr <git@qaq.wiki>
2025-11-04 14:33:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,7 @@
 set -euo pipefail
 
 cd "$(dirname "$0")"
-chmod +x *.sh
-chmod +x *.py
+chmod +x ./*
 cp -f yolo_*.sh /usr/local/bin/
 cp -f yolo_*.py /usr/local/bin/
 
-./yolo_tart_prepare.sh
-
-echo "[*] installation complete. You can now run 'yolo_claude.sh', 'yolo_codex.sh', or 'yolo_zsh.sh' from anywhere."
